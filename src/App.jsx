678b270bae750e2435b2d3a6eb76c31d2bd34ea9
Mustafa <mu88ali@gmail.com>
2025-08-05 13:34:02 +0200

    feat:created project,  install and configure Material UI (MUI) in the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import './index.css'
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Hello, MUI 👋</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
